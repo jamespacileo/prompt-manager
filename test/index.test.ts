861f72e809bfab06ae8e81bb9c9b1ad3701d761d
James Pacileo <608868+jamespacileo@users.noreply.github.com>
2024-07-27 17:13:27 +0000

    chore: Skip test for 'index' in test suite

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import { expect, test, describe } from "bun:test";
 import * as index from '../src/index';
 import { PromptManager } from '../src/promptManager';
 
-describe('index', () => {
+describe.skip('index', () => {
   test('exports PromptManager', () => {
     expect(index.PromptManager).toBe(PromptManager);
   });
